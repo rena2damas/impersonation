a6e073d6032dd7001d20bf47e4bdc3ecec1b7320
renatodamas <rena2damas@gmail.com>
2022-09-26 17:06:33 +0100

    refactor: add __all__ #minor

--- src/impersonation/__init__.py
+++ src/impersonation/__init__.py
@@ -2,5 +2,7 @@ try:
     from importlib import metadata
 except ImportError:
     import importlib_metadata as metadata  # python<=3.7
+from .core import impersonate
 
+__all__ = (impersonate,)
 __version__ = metadata.version("impersonation")
